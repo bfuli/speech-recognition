5f117724a86f85e0d6b422fba2efbbb4fb631ae5
bfl <2405720158@qq.com>
2021-09-22 16:56:49 +0800

    commit

--- src/audio_converter.py
+++ src/audio_converter.py
@@ -12,6 +12,8 @@ def get_name(audio_path):
 def m4a2wav(audio_path, dest_path):
     """
     将m4a格式的语音文件转成wav格式，转换后的文件名与转换前相同，知识后缀不同
+    注意：该方法需要ffmpeg.exe、ffplay.exe、ffprobe.exe，3个文件支持，需自行下载该文件
+         并将文件所在目录加入到系统环境变量
     :param audio_path: 原语音文件位置
     :param dest_path: 转换后的文件目录
     :return:
